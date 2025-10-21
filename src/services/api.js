d43734b892c4182a5233435fcae5c991fc2437ea
jeraldstrinidad <jst_6black6metal6@yahoo.com>
2025-10-21 14:46:17 +0800

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://be-hotelbooking.vercel.app/api',
+  baseURL: 'https://be-hotelbooking.vercel.app/api',
   headers: { Accept: 'application/json' }
 })
 
